d4c628a5f979496b5974c34d12b868011a205dc7
Rizki Fajar <muhrizkifajar28@gmail.com>
2024-01-31 20:14:45 +0700

    feat(customer): add get all customer route api

--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::post('customers', [CustomerController::class, 'create']);
 
 Route::put('customers/{id}', [CustomerController::class, 'update']);
+
+Route::get('customers', [CustomerController::class, 'getAll']);
