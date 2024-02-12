a8b0f8d919d20b4352f1eb9ce22b580c64b9f807
sam <sheila.zahra2310@gmail.com>
2024-02-12 12:58:56 +0700

    memperbaiki tampilan dashboard tambah dan edit

--- resources/views/admin/dashboardEdit.blade.php
+++ resources/views/admin/dashboardEdit.blade.php
@@ -9,15 +9,15 @@
            Edit Pelanggan 
         </p>
         <div class="form-group">
-        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder=" ">
+        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Bu Istiqomah">
         <label for="exampleFormControlInput1">Nama</label>
       </div>
       <div class="form-group">
-        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder=" ">
+        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="081111111111">
         <label for="exampleFormControlInput1">Nomor Telepon</label>
       </div>
       <div class="form-group">
-        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder=" ">
+        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="081111111111">
         <label for="exampleFormControlInput1">Alamat</label>
       </div>
       <button type="button" class="button-tambah">Update</button>
